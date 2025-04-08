2f0fd366442deffd67f241a55dce9c0e4a5332c1
Matt Radford <quinquex@gmail.com>
2025-04-07 17:34:58 -0700

    init db

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cave(models.Model):
+    name = models.CharField(max_length=100)
+    rate = models.IntegerField()
+    sleeps = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    zipcode = models.IntegerField()
+    description = models.TextField(max_length=500)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
