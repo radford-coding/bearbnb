8526c91bee5918322b1b6d5a1898b7233ac5160d
Matt Radford <quinquex@gmail.com>
2025-04-07 17:04:23 -0700

    about/ route

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 class Home(LoginView):
     template_name = 'home.html'
+
+class About(TemplateView):
+    template_name = 'about.html'
