115363cf69c31752c7c69da62cd4845f5288a123
Matt Radford <quinquex@gmail.com>
2025-04-11 15:21:33 -0700

    basic functionality done

--- main_app/static/js/helpers.js
+++ main_app/static/js/helpers.js
@@ -5,3 +5,11 @@ const nightsEl = document.querySelector('input[name="nights"]');
 nightsEl.addEventListener('change', (e) => {
     priceEl.innerText = e.target.value * Number(rateEl.innerText);
 });
+
+const uploadEl = document.querySelector('#file-upload');
+const displayUploadEl = document.querySelector('#uploaded-file-name');
+
+uploadEl.addEventListener('change', (e) => {
+    let path = e.target.value.split('\\');
+    displayUploadEl.innerText = path[path.length - 1];
+});
